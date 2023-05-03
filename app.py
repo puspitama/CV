bd62b58ef7de94a8cf15747d54e9c1dc5800dafb
“puspitama” <dy.dyahayu022@gmail.com>
2023-05-03 14:20:34 +0700

    revisi

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 from flask_ngrok import run_with_ngrok
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 
 @app.route("/")
 def halamanCV():
     return render_template("index.html")
 
-if _name_ == "_main_":
+if __name__== "__main__":
     run_with_ngrok(app)
-    app.run()
+    app.run()
